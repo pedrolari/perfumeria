a1dee0d46e15142ef49966f3642e2bb419e7dda6
emilio86 <epascualg@salesianospizarrales.com>
2017-11-20 17:05:15 +0100

    Clase conexion
    Clase conexion creada, con metodos de consulta y modificacion

---
+++ src/Conexion.java
@@ -0,0 +1,36 @@
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexion {
+	private Connection con;
+	Conexion() throws SQLException, ClassNotFoundException{
+		Class.forName("com.mysql.jdbc.Driver");
+		String url = "jdbc:mysql://localhost/perfumeria";
+		con = DriverManager.getConnection(url,"root","");
+	}
+	public Connection getCon() {
+		return con;
+	}
+	public void close() throws SQLException{
+		con.close();
+	}
+	//devuelve resulset con la consulta que se le pasa como string en el argument0
+	//ejemplo a la hora de llamar: c.consultar("select * from articulos);"
+	public ResultSet consultar(String consulta) throws SQLException{
+		Statement stm = con.createStatement();
+		ResultSet rs = stm.executeQuery(consulta);
+		return rs;
+	}
+	//para insert,update, se le pasa string de sql
+	//ejemplo a la hora de llamar: c.modificar("insert into articulos where id=1");
+	public void modificar(String update) throws SQLException{
+		Statement stm = con.createStatement();
+		stm.executeUpdate(update);
+	}
+}
